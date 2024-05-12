039a8da102058e5730ad814aec3953c7a6a4a44a
Arpad <arpad@Arpads-MacBook-Pro.local>
2024-05-12 16:34:15 -0400

    add post request

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import logo from './logo.svg';
-import {
-  BlogCreateForm,
-  BlogUpdateForm,
-  CommentCreateForm,
-  CommentUpdateForm,
-  PostCreateForm,
-  PostUpdateForm
-  // studioTheme
-} from './ui-components'
-import './App.css';
+import { post } from 'aws-amplify/api'
+import './App.css'
+
+async function postBlog(id) {
+  try {
+    const restOperation = post({
+      apiName: 'blogApi',
+      path: `/blog/${id}`,
+      options: {
+        body: {
+          message:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }
+      }
+    })
+
+    const { body } = await restOperation.response
+    const response = await body.json()
+
+    console.log('POST call succeeded')
+    console.log(response)
+  } catch (e) {
+    console.log('POST call failed: ', JSON.parse(e.response.body))
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
-        <BlogCreateForm />
-        <BlogUpdateForm />
-        <CommentCreateForm />
-        <CommentUpdateForm />
-        <PostCreateForm />
-        <PostUpdateForm />
-      </header>
+      <button onClick={postBlog}>Post Todo</button>
     </div>
   )
 }
 
-export default App;
+export default App
